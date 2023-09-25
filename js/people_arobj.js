d182725a555e6e3398007f4ded8c191d4294e08c
Suma Varshitha <134911827+SumaVarshitha6@users.noreply.github.com>
2023-09-25 12:04:46 +0530

    Add files via upload

---
+++ js/people_arobj.js
@@ -0,0 +1,66 @@
+var people = [
+  {
+  "firstname": "praveen",
+  "lastname": "gubbala",
+  "age": 36,
+  "gender": "male",
+  "city": "hyd",
+  "salary": 10000
+  },
+  {
+  "firstname": "srikanth",
+  "lastname": "gubbala",
+  "age": 32,
+  "gender": "male",
+  "city": "bengaluru",
+  "salary": 20000
+  },
+  {
+  "firstname": "pradeep",
+  "lastname": "reddy",
+  "age": 21,
+  "gender": "male",
+  "city": "hyd",
+  "salary": 25000
+  },
+  {
+  "firstname": "mounika",
+  "lastname": "mudiraj",
+  "age": 20,
+  "gender": "female",
+  "city": "nalgonda",
+  "salary": 30000
+  },
+  {
+  "firstname": "nikhil",
+  "lastname": "m",
+  "age": 22,
+  "gender": "male",
+  "city": "guntur",
+  "salary": 2000
+  },
+  {
+  "firstname": "riya",
+  "lastname": "bhadouria",
+  "age": 14,
+  "gender": "female",
+  "city": "indore",
+  "salary": 40000
+  }
+  ];
+ /* for(var f in people){ 
+    if((people[f].firstname).startsWith("p")){
+    console.log((people[f].firstname.toUpperCase()).concat(" ",(people[f].lastname)));
+    }
+  } */
+/* people.forEach(function(value){//all objs
+  console.log(value);
+}); */
+ /* people.forEach(function(value){//all firstnames
+  console.log(value.firstname);
+}); */
+
+
+
+       
+      
