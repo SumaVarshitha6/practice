e151bd9162d9b740125ef4f19684d9a848fe3e54
Suma Varshitha <134911827+SumaVarshitha6@users.noreply.github.com>
2023-09-25 11:59:31 +0530

    Add files via upload

---
+++ js/test1.js
@@ -0,0 +1,93 @@
+//72.Find the first occurrence of a character in a given string.
+/*var str="chocolate is ok and good";
+console.log(str.indexOf("ok"));*/
+//73.Find the last occurrence of a character in a given string.
+/*var str="chocolate is ok and good ok";
+console.log(str.lastIndexOf("ok")); */
+//74.Search all occurrences of a character in a given string.
+//75.Count occurrences of a character in a given string.
+var str="chocolate is ok and good ok";
+console.log(str.lastIndexOf("o"));
+//76.Find the highest frequency character in a string.
+//77.Find the lowest frequency character in a string.
+//78.Count the frequency of each character in a string.
+//79.Remove the first occurrence of a character from a string.
+/*var str="chocolate is ok and good ok";
+var a=str.indexOf("o");
+str=remove.indexof('o');
+console.log(str);*/
+//80.Remove the last occurrence of a character from a string.
+//81.Delete all occurrences of a character from a string.
+//82.Remove all repeated characters from a given string.
+//83.Replace the first occurrence of a character with another in a string.
+//84.Replace the last occurrence of a character with another in a string.
+//85.Put all occurrences of a character with another in a string.
+//86.Find the first occurrence of a word in a given string.
+
+//87.Find the last occurrence of a word in a given string.
+//88.Search all occurrences of a word in a given string.
+//89.Count occurrences of a word in a given string.
+//90.Remove the first occurrence of a word from the string.
+//91.Remove the last occurrence of a word in a given string.
+//92.Delete all occurrence of a word in a given string.
+//93.A Trim leading white space characters from a given string.
+//94.Trim trailing white space characters from a given string.
+//95.Trim both leading and trailing white space characters from a given string.
+//96.Remove all extra blank spaces from the given string.
+//97.A String is Palindrome or Not
+//99.A String Is an Anagram or Not
+//100.Find the length of a string.
+//101.Copy one string to another string.
+//102.Concatenate two strings.
+//103.Compare two strings.
+//104.Convert lowercase string to uppercase.
+//105.Convert uppercase string to lowercase.
+//106.Toggle case of each character of a string.
+//107.Find a total number of alphabets, digits or special character in a string.
+//108.Count the total number of vowels and consonants in a string.
+//109.Count the total number of words in a string.
+//110.Find the reverse of a string.
+//111.Check whether a string is a palindrome or not.
+//112.Reverse order of words in a given string.
+//113.Reverse of a number using toString, split, reverse and join methods.
+//114.Elimination of zeroes from a given number.
+//115.Find the number of digits of a given number using toString() and length property
+//116.Write a program to take a string and print only the numeric characters using. Example: Input->‘Q1STR5684AK’; Output->‘15684’
+//117.Search for a given number in an array
+//118.Print the number of words in a given string
+//119.Remove the single spaces from a given sentence
+//120.remove all the spaces from a given string
+//121.Search a given string in a sentence(both uppercase and lowercase)
+//122.Write a program to convert a sample sentence string into an array of all the words in the sentence. Example: Input->‘Hello World’; Output->[‘Hello’,‘World’]
+//123.Write a program to eliminate all numeric characters from a string. Example: Input->‘Q1STR5684A’; Output-> ‘QSTRAK’
+//124.Write a program to take a string and print only the numeric characters. Example: Input->‘Q1STR5684AK’; Output->‘15684’
+//125.Write a program to take a string and print the number of characters in the string. Example: Input->‘Hello’; Output->5
+//126.Write a program to take a 16-digit credit card number and replace the first 12 characters with ‘X’. Example:Input->‘9765143265387960’; Output->‘XXXXXXXXXXXX7960’
+//127.Write a program to take a string and replace all vowels with ‘8’. Example: Input->‘This is awesome’; Output->‘Th8s 8s 8w8s8m8’
+//128.Write a program to hide the middle six digits of a phone number. Example: Input-> '9876543210'; Output-> '98XXXXXX10'
+//129.Append the calling code of India(+91) to a given phone number. Example: Input-> '9876543210'; Output-> '+919876543210'
+//130.Check whether a given number is a valid phone number.
+    //    Example:Input-> '7654893274'; Output-> 'valid'
+     //   Input-> '785487438754'; Output-> 'invalid'.........(must be 10 digits)
+      //  Input-> '7854s54839'; Output-> 'invalid'.............(must NOT contain letters)
+    
+//131.Check whether a given number is a valid credit card number. Example:
+    //    Example:
+     //   Input-> '7463836483647454'; Output-> 'valid'
+      //  Input-> '74638364836474548'; Output-> 'invalid'...(must be 16 digits)
+       // Input-> '7463836w4836o745'; Output-> 'invalid'...(must contain only numbers)
+    
+//132.Write a program to add hyphens (-) in between a given credit card number.
+      //  Example:
+        //Input-> '6484638463487486'; Output-> '6484-6384-6348-7486'
+                        
+//133.Sort a given string
+        // Example: ‘praveen’=>’aeenprv’
+                        
+//134.Eliminate Duplicate characters in given string
+                        
+       // Example: ‘praveengubbala’ => ‘pravengubl’
+                        
+//135.Write a program to print only numbers out of a given string
+      //  Example: let us take pr34s2v9q4
+        //The output should be 3,4,2,9,4
